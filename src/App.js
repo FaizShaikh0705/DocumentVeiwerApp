f02ab4a8d5952ec8c5dc6d148691cfc405d3b659
FaizShaikh0705 <faizshaikh0705@gmail.com>
2023-05-13 05:41:39 +0530

    deleted unwanted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Viewpdf from './components/ViewPdf/Viewpdf'
-import Doctohtml from './components/ViewPdf/DocxtoHtml/Doctohtml'
 
 const App = () => {
   return (
     <>
     <Viewpdf/>
-    {/* <Doctohtml /> */}
     </>
   )
 }
